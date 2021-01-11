72920b7c874e521ac578689d0b867565c7e956a3
Dane <wb-jbn819562@cainiao.com>
2021-01-11 22:08:00 +0800

    chore: 使用 eslint 、husky 提高代码质量

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  globals: {
+    it: 'readonly',
+    expect: 'readonly',
+    describe: 'readonly'
+  },
+  extends: 'standard',
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
