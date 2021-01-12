53ad04e5189bc36b004776ddc74465dbbae6d88d
Dane <wb-jbn819562@cainiao.com>
2021-01-12 16:37:42 +0800

    chore: 单文件测试时，代码覆盖率只检测此文件

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,19 @@
-export default {
-  transform: {},
-  collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
+import path from 'path';
+
+export default () => {
+  const file = process.argv.find(arg => /\.test\.js$/.test(arg));
+  return {
+    rootDir: './',
+    transform: {},
+    collectCoverage: true,
+    collectCoverageFrom: file ? [path.relative('./', file.replace(/\.test\.js$/, '.js'))] : null,
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100
+      }
     }
-  }
+  };
 };
